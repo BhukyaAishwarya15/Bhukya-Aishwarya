d6616042fa6b3f4517a30521a29b196903c14699
BhukyaAishwarya15 <22je0250@iitism.ac.in>
2024-08-11 11:00:17 +0530

    commit6_adding a example

--- finding_external pressure_in_radialflow_of_incompressible_fluids.py
+++ finding_external pressure_in_radialflow_of_incompressible_fluids.py
@@ -1,5 +1,5 @@
 import math
-print("Reservoir Performance : Therotical Method")
+print("Reservoir Performance : Theorotical Method")
 print("radial flow of incompressible fluids")
 print("\n")
 
@@ -14,7 +14,7 @@ print("\n")
 print("Where:")
 
 
-print("Qo = oil,flow rate,STB/day")
+print("Qo = oil flow rate,STB/day")
 print("Pe = external pressure,psi")
 print("Pwf = bottom-hole flowing pressure ,psi")
 print("k = permeability,md")
@@ -24,23 +24,25 @@ print("h = thickness,ft")
 print("re = external or drainage radius,ft")
 print("rw = wellbore radius ,ft")
 print("\n")
-print("regime = steady state")
-print("Fluid = incompressible")
 
 #Example
 #Qo=600 stb/day,k=120 md,h=25ft,Pwf=1800 psi,μo=2.5cp,Bo=1.25 bbl/STB,re=745 ft,rw=0.25 ft
 #Pe = Pwf + [μoBoQo/0.00708kh] ln(re/rw)
+#regime=steady state , fluid type= incompressible
 #Pe=2506 psi
 
 
-Qo = float(input("Qo="))
-k = float(input("k="))
-h = float(input("h="))
-Pwf = float(input("pwf="))
-μo = float(input("μo="))
-Bo =float(input("Bo="))
-re = float(input("re="))
-rw = float(input("rw="))
+print("regime = steady state")
+print("Fluid = incompressible")
+Qo = float(input("Enter the value of Qo = "))
+k = float(input("Enter the value of k= "))
+h = float(input("Enter the value of h="))
+Pwf = float(input("Enter the value of Pwf="))
+μo = float(input("Enter the value of μo="))
+Bo =float(input("Enter the value of Bo="))
+re = float(input("Enter the value of re="))
+rw = float(input("Enter the value of rw="))
 print("pe=" ,Pwf + (((μo*Bo*Qo)/(0.00708*k*h)) * math.log(re/rw)))
+
 
 
